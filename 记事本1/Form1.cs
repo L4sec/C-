6f79ab4bdd5fcb4c887ac20cd4b96815e5c56181
unknown <anonymousp3@hotmail.com>
2019-04-24 15:54:19 +0800

    c#

---
+++ 记事本1/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace 记事本1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //加载程序的时候隐藏panel
+            panel1.Visible = false;
+            //取消文本框自动换行
+            textBox1.WordWrap = false;
+        }
+        /// <summary>
+        /// 点击按钮隐藏panel
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+        }
+
+        private void 显示ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+        }
+
+        private void 隐藏ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+        }
+        List<string> list = new List<string>();
+        /// <summary>
+        /// 打开对话框
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void 打开ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Title = "请选择要打开的文件";
+            open.InitialDirectory = @"C:\Users\长者\Desktop";
+            open.Multiselect = true;
+            open.Filter = "文本文件|*.txt|所有文件|*.*";
+            open.ShowDialog();
+            //获得用户选中的路径
+            string path = open.FileName;
+            //将文件路径全部存到泛型集合中
+            list.Add(path);
+            //获得用户打开文件的文件名
+            string fileName = Path.GetFileName(path);
+            //将文件名放到Listbox中
+            listBox1.Items.Add(fileName);
+            if(path == "")
+            {
+                return;
+            }
+            using (FileStream fsRead = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Read))
+            {
+                byte[] buffer = new byte[1024 * 1024 * 5];
+                int r = fsRead.Read(buffer, 0, buffer.Length);
+                textBox1.Text = Encoding.Default.GetString(buffer, 0, r);
+
+            }
+        }
+        /// <summary>
+        /// 保存对话框
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void 保存ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.InitialDirectory = @"C:\Users\长者\Desktop";
+            save.Title = "请选择要保存的文件路径";
+            save.Filter = "文本文件|*.txt|所有文件|*.*";
+            save.ShowDialog();
+
+            //获得用户保存路径
+            string path = save.FileName;
+            if (path == "")
+            {
+                return;
+            }
+            using (FileStream fsWrite = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                byte[] buffer = Encoding.Default.GetBytes(textBox1.Text);
+                fsWrite.Write(buffer, 0, buffer.Length);
+            }
+            MessageBox.Show("保存成功");
+        }
+
+        private void 自动换行ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(自动换行ToolStripMenuItem.Text=="自动换行")
+            {
+                textBox1.WordWrap = true;
+                自动换行ToolStripMenuItem.Text = "取消自动换行";
+            }
+            else if(自动换行ToolStripMenuItem.Text=="取消自动换行")
+            {
+                textBox1.WordWrap = false;
+                自动换行ToolStripMenuItem.Text = "自动换行";
+            }
+        }
+
+        private void 字体ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FontDialog zt = new FontDialog();
+            zt.ShowDialog();
+            textBox1.Font = zt.Font;
+        }
+
+        private void 颜色ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog ys = new ColorDialog();
+            ys.ShowDialog();
+            textBox1.ForeColor = ys.Color;
+        }
+
+        private void 文件ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 双击打开对应的文件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            //要获得双击的文件所对应的全路径
+            string path = list[listBox1.SelectedIndex];
+            using (FileStream fsRead = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Read))
+            {
+                byte[] buffer = new byte[1024 * 1024 * 5];
+                int r = fsRead.Read(buffer, 0, buffer.Length);
+                textBox1.Text = Encoding.Default.GetString(buffer, 0, r);
+
+            }
+        }
+    }
+}
